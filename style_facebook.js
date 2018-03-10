3adfdda4bdf9072f4b4987425c71f11dd749b3fa
Ananya Agrawal <33188930+ananya-agrawal@users.noreply.github.com>
2018-03-11 03:36:02 +0530

    Update style_facebook.js

--- style_facebook.js
+++ style_facebook.js
@@ -1,11 +1,9 @@
 let a = 1;
-let f = 1;
 
 var userData = [];
 setInterval(function () {
-  updateMinimal();
+  updateBackgroundSettings();
 }, 1000);
-
 
 window.onresize = function () {
   autoWidth();
@@ -20,41 +18,32 @@ function autoWidth() {
 function updateMinimal() {
   // status-icon  Minimal Mode
 
+  // chrome.storage.local.get(/* String or Array */["statusicon"], function (
+  //   items
+  // ) {
+  //   if (items.statusicon === "true") {
 
-  chrome.storage.local.get(/* String or Array */["statusicon"], function (
-    items
-  ) {
-    if (items.statusicon === "true") {
+  //     if (a === 1) {
+  //       $(function () {
 
-      if (a === 1) {
-        $(function () {
+  //         $('#bottomContent').html("<img src='https://source.unsplash.com/random/1000x470' style='margin-left:-20px'/>")
+  //         a = 2;
+  //       });
+  //     }
+  //     updateBackgroundSettings();
 
-          $('#bottomContent').html("<img src='https://source.unsplash.com/random/1000x470' style='margin-left:-20px'/>")
-          a = 2;
-        });
-      }
-      updateBackgroundSettings();
+  //   }
+  //   else if (items.statusicon === "false") {
+  //     if (a === 2) {
+  //       location.reload();
+  //       a = 1;
+  //     }
+  //     // Add focus mode
 
-
-    }
-    else if (items.statusicon === "false") {
-      if (a === 2) {
-        location.reload();
-        a = 1;
-      }
-      // Add focus mode
-
-
-
-
-
-    }
-  });
-
-
+  //   }
+  // });
 
 }
-
 function updateBackgroundSettings() {
   if ($("body").hasClass("UIPage_LoggedOut")) return;
   chrome.extension.sendMessage({ method: "get_vars" }, function (response) {
@@ -70,24 +59,63 @@ function updateBackgroundSettings() {
         $("._5pcb._dp7._4j3f").css("display", "none");
         $("._2t-f").css("display", "none");
         $(".bottomContent").css("color", "white");
+        $('.home_right_column').css("display", "none");
 
-
-
+        $('.uiFutureSideNav').css("display", "none");
+        $('._64b').css("display", "none");
+      
+        if (a === 1) {
+          $(function () {
+  
+            $('#bottomContent').html("<img src='https://source.unsplash.com/random/1000x470' style='margin-left:-20px'/>")
+            a = 2;
+          });
+        }  
       }
       else if (items.onoffswitch === "false") {
         // console.log("it worked");
         $("._5pcb._dp7._4j3f").css("display", "inline");
         $("._2t-f").css("display", "inline");
         $('._2s25').css('color', 'white');
+        if (a === 2) {
+          location.reload();
+          a = 1;
+        }
       }
     });
 
+     //minimal mode
 
+  chrome.storage.local.get(/* String or Array */["statusicon"], function (
+    items
+  ) {
+    if (items.statusicon === "true") {
+      $('.home_right_column').css("display", "none");
+      
+			$('.fixed_elem').css("display", "none");
+      $('._64b').css("display", "none");
+      // if (a === 1) {
+      //   $(function () {
+
+      //     $('#bottomContent').html("<img src='https://source.unsplash.com/random/1000x470' style='margin-left:-20px'/>")
+      //     a = 2;
+      //   });
+      // }
+      updateBackgroundSettings();
+
+    }
+    else if (items.statusicon === "false") {
+      // if (a === 2) {
+      //   location.reload();
+      //   a = 1;
+      // }
+      // Add focus mode
+
+    }
+  });
 
     $('._15p4._2pis').css('color', 'black');
     if (userData[2] != " ") {
-
-
 
       $('#pagelet_navigation').css('display', 'none');
       $('#leftCol').css('display', 'none');
@@ -99,7 +127,6 @@ function updateBackgroundSettings() {
       $('._1-ia').css('display', 'none');
 
       $('#pagelet_ego_pane').css('display', 'none');
-
 
       //background set
       if (!$("#chromeFacebookbackground").length) {
